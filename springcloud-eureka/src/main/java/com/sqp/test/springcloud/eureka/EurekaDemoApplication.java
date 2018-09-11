e2f521f8f1206a4afed4bee92c2b7a92229a7691
sunquanpeng <sunqp000@126.com>
2018-09-11 11:01:51 +0800

    添加测试

---
+++ springcloud-eureka/src/main/java/com/sqp/test/springcloud/eureka/EurekaDemoApplication.java
@@ -0,0 +1,9 @@
+package com.sqp.test.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+
+public class EurekaDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDemoApplication.class,args);
+    }
+}
